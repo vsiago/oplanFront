39ed2cb63e9a8648a0c64940c4a456fc7fd99eb8
vsiago <vsiago22@gmail.com>
2024-01-10 16:22:46 -0300

    Feat: SC noticias etapa 1 ok

--- src/components/SCNoticias.tsx
+++ src/components/SCNoticias.tsx
@@ -1,31 +1,37 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Swiper from "react-native-swiper"
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function SCNoticias() {
 
   return (
     <>
       <Text style={styles.text}>notícias</Text>
-      <View style={styles.swiperContainer}>
-      <Swiper
-        autoplay={false}
-        loop={true}
-        paginationStyle={{ bottom: -25 }}
-        dotColor="#50596B"
-        activeDotColor="#31A7E5"
+      <LinearGradient
+        // Background Linear Gradient
+        colors={['#4F5F81', 'rgba(79, 95, 129, .4)']}
+        style={styles.swiperContainer}
       >
-        <View style={styles.slide}>
-
-        </View>
-        <View style={styles.slide}>
-          <Text>Slide 2</Text>
-        </View>
-        <View style={styles.slide}>
-          <Text>Slide 3</Text>
-        </View>
-      </Swiper>
-    </View>
+        <Swiper
+            autoplay={false}
+            loop={true}
+            paginationStyle={{ bottom: -25 }}
+            dotColor="#50596B"
+            activeDotColor="#31A7E5"
+        >
+            <View style={styles.slide}>
+                <Text>Slide 1</Text>
+            </View>
+            <View style={styles.slide}>
+                <Text>Slide 2</Text>
+            </View>
+            <View style={styles.slide}>
+                <Text>Slide 3</Text>
+            </View>
+        </Swiper>
+        </LinearGradient>
     </>
   );
 }
@@ -45,12 +51,13 @@ const styles = StyleSheet.create({
     height: 250,
     paddingBottom: 24,
     paddingHorizontal: 20,
+    marginHorizontal: 20,
+    borderRadius: 20
   },
   slide: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#4F5F81",
     height: 400,
     borderRadius: 10,
   },
