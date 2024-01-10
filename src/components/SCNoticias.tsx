33fc9e1833b1f62f68d2bc2f12175cc6f91ec036
vsiago <vsiago22@gmail.com>
2024-01-10 16:46:41 -0300

    Feat: Etapa 3 SC Noticias ok

--- src/components/SCNoticias.tsx
+++ src/components/SCNoticias.tsx
@@ -9,34 +9,45 @@ export default function SCNoticias() {
   return (
     <>
       <Text style={styles.text}>notícias</Text>
-      <LinearGradient
-        // Background Linear Gradient
-        colors={['#4F5F81', 'rgba(79, 95, 129, .4)']}
-        style={styles.swiperContainer}
-      >
         <Swiper
             autoplay={false}
             loop={true}
-            paginationStyle={{ bottom: -25 }}
+            paginationStyle={{ bottom: 20 }}
             dotColor="#50596B"
             activeDotColor="#31A7E5"
+            style={styles.swiperContainer}
         >
-            <View style={styles.slide}>
-                <Text>Slide 1</Text>
+            <View >
+                <LinearGradient style={styles.slide} colors={['#4F5F81', 'rgba(79, 95, 129, .5)']}>
+                    <Text>Slide 1</Text>
+                </LinearGradient>
             </View>
-            <View style={styles.slide}>
-                <Text>Slide 2</Text>
+            <View>
+                <LinearGradient style={styles.slide} colors={['#4F5F81', 'rgba(79, 95, 129, .5)']}>
+                    <Text>Slide 2</Text>
+                </LinearGradient>
             </View>
-            <View style={styles.slide}>
-                <Text>Slide 3</Text>
+            <View>
+                <LinearGradient style={styles.slide} colors={['#4F5F81', 'rgba(79, 95, 129, .5)']}>
+                    <Text>Slide 3</Text>
+                </LinearGradient>
             </View>
         </Swiper>
-        </LinearGradient>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+swiperContainer: {
+    height: 300,
+},
+slide: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 250,
+    borderRadius: 20,
+    marginHorizontal: 20,
+  },
   text: {
     color: "#A0B3CF",
     textTransform: "uppercase",
@@ -46,19 +57,5 @@ const styles = StyleSheet.create({
     paddingHorizontal: 35,
     marginTop: 40,
     marginBottom: 15
-  },
-  swiperContainer: {
-    height: 250,
-    paddingBottom: 24,
-    paddingHorizontal: 20,
-    marginHorizontal: 20,
-    borderRadius: 20
-  },
-  slide: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    height: 400,
-    borderRadius: 10,
   },
 });
