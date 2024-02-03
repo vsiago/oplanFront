0128b097e068beba5e3fc291789971dc92e0747f
vsiago <101620032+vsiago@users.noreply.github.com>
2024-02-02 21:58:44 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "",
-  baseURL: "http://192.168.7.110:3131/api",
+  baseURL: "https://api-kappa-red.vercel.app//api",
 });
 
 export { api };
